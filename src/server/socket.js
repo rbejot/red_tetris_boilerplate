bd2bb831b292c4e6541402850c8ad89bd640f889
Maud BUCHWALTER <mabuchwa@e2r6p9.42.fr>
2018-05-25 16:34:11 +0200

    modify emit object

--- src/server/socket.js
+++ src/server/socket.js
@@ -14,24 +14,34 @@ export const initEngine = (io,loginfo) => {
         let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0
         console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
         if(numClients === 0){
-          socket.join(room)
+          socket.join()
           console.log('Client ID ' + socket.id + ' joined room ' + room);
-          socket.emit('action', {type: 'create', room: 'room', id: socket.id})
+          socket.emit('action', {type: 'create', room: room, id: socket.id})
           generate_tetri(socket.id)
-      } else if(numClients === 1){
-          console.log('Client ID ' + socket.id + ' joined room ' + room);
-          io.sockets.in(room).emit('join', room)
-          socket.join(room)
-          socket.emit('action', {type: 'joined', room: 'room', id: socket.id})
-          io.sockets.in(room).emit('ready');
-      } else {
-        socket.emit('action', {type: 'full', room: 'room'})
-        console.log('this room is full')
+        } else {
+          socket.emit('action', {type: 'reject', room: room})
+          console.log('this room is full')
+        }
       }
+      if (action.type === 'server/join_room'){
+        let room = action.room
+        let clientsInRoom = io.sockets.adapter.rooms[room]
+        let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0
+        if(numClients === 1){
+            console.log('Client ID ' + socket.id + ' joined room ' + room);
+            socket.join(room)
+            socket.emit('action', {type: 'joined', room: room, id: socket.id})
+            io.sockets.in(room).emit('ready');
+        } else {
+          socket.emit('action', {type: 'reject', room: room})
+          console.log('this room is full')
+        }
       }
     })
   })
 }
+
+
 
 const generate_tetri = id => {
   console.log(id)
