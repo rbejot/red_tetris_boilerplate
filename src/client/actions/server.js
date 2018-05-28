3757935a9d836e277618b019be72d6b12a1b7c39
Maud BUCHWALTER <mabuchwa@e2r6p9.42.fr>
2018-05-28 16:35:27 +0200

    resolve merge conflict"

--- src/client/actions/server.js
+++ src/client/actions/server.js
@@ -2,15 +2,3 @@ export const ping = () => ({
   type: 'server/ping',
   data: 'Ping!'
 })
-
-<<<<<<< Updated upstream
-// export const create_room = () => ({
-//   type: 'server/create_room',
-//   room: 'd435g'
-// })
-=======
-export const create_room = () => ({
-  type: 'server/create_room',
-  room: 'd435g'
-})
->>>>>>> Stashed changes
