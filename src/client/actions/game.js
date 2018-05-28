2cffb1a7006dbb4382c7a0ca6671718a5b3e8714
Rodolphe <rbejot@student.42.fr>
2018-05-28 16:38:24 +0200

    username begin submit

--- src/client/actions/game.js
+++ src/client/actions/game.js
@@ -1,8 +1,13 @@
 export const CREATE_ROOM = 'server/create_room'
+export const ADD_USERNAME = 'add_username'
 
 export const create_room = (room, player) => ({
   type: CREATE_ROOM,
-  room: room,
-  player: player
+  room: room
+})
+
+export const add_username = (username) => ({
+  type: ADD_USERNAME,
+  player: username
 })
 
--- src/client/reducers/alert.js
+++ src/client/reducers/alert.js
@@ -1,14 +1,19 @@
 import { ALERT_POP } from '../actions/alert'
-import { CREATE_ROOM } from '../actions/game'
+import { CREATE_ROOM, ADD_USERNAME } from '../actions/game'
 
 const reducer = (state = {} , action) => {
   switch(action.type){
     case ALERT_POP:
       return { message: action.message }
+    case ADD_USERNAME:
+      return {
+        player: action.player
+      }
     case 'reject':
       return { party: false }
     case 'create':
-      return { 
+      return {
+        ...state,
         party: true,
         master: true,
         full: false,
