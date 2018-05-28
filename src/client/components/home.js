cbcb405e35dacbf375bcd19e2b8c635687fb8101
Rodolphe <rbejot@student.42.fr>
2018-05-28 15:09:07 +0200

    fix button twice and redirect if no state.room

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { Link, Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
-const Home = ({props, actions, state, room, player}) => {
-    console.log(props)
-    console.log(actions)
-    console.log(state)
+const Home = ({props, actions, state, player}) => {
+    // console.log(props)
+    // console.log(actions)
+    // console.log(state)
+    const generate_room = () => {
+      let text = "";
+      let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for (let i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      return text;
+    }
+    const room = generate_room()
+    console.log(room)
     return (
       <div>
         <input id="userName" type="text"/>
-        <button><Link to={`/${room}[${player}]`} onClick={() => actions.create_room(room, player)}>Create room</Link></button>
+        {state.room ? <Redirect to={`/${state.room}[${player}]`}/> : "" }
+        <button onClick={() => actions.create_room(room, player)}>Create room</button>
       </div>
     )
 }
--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -5,25 +5,17 @@ import * as Actions from '../actions/game'
 import * as Server from '../actions/server'
 import Board from '../components/board'
 import Home from '../components/home'
-import { HashRouter, Route, Link, Redirect, Switch } from 'react-router-dom'
+import { HashRouter, Route, Redirect } from 'react-router-dom'
 
 // GET_ROOM pour creer liste de room possible
 
 const App = ({state, actions}) => {
-  const generate_room = () => {
-    let text = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < 5; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-  }
-  const room = generate_room()
   const player = "toto"
   return (
     <HashRouter hashType="noslash">
     <div>
       <Route exact path="/" render={(props) => (
-        <Home props={props} actions={actions} state={state} room={room} player={player}/>
+        <Home props={props} actions={actions} state={state} player={player}/>
       )}/>
       <Route exact path="/:room[:player]" render={(props) => (
         state.party ? (
